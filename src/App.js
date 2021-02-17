d63958625b877b2e6b6dff0df25cacbe2737c689
allexib <ooomegasib@gmail.com>
2021-02-17 18:39:02 +0400

    widget 28\55,useEffect Cleanup Function

---
+++ src/App.js
@@ -0,0 +1,26 @@
+import React from 'react';
+import Accordion from './components/Accordion';
+import Search from './components/Search';
+
+const items = [
+    {
+        title: 'What is React?',
+        content: 'React is a front end js framework'
+    },
+    {
+        title: 'Why use React',
+        content: 'React is a favorite js library among engineers'
+    },
+    {
+        title: 'How do you use React?',
+        content: 'You use React by creating components'
+    }
+]
+
+export default () => {
+    return (
+        <div>
+            <Search/>
+        </div>
+    );
+};
