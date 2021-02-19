c73694ffe600f441c5bef128027f7c656e2699d3
allexib <ooomegasib@gmail.com>
2021-02-19 14:38:57 +0400

    widget 8\10 203 Detecting Navigation

--- src/components/Link.js
+++ src/components/Link.js
@@ -4,6 +4,9 @@ const Link = ({className, href, children}) => {
     const onClick = (event) => {
         event.preventDefault();
         window.history.pushState({}, '', href);
+
+        const navaEvent = new PopStateEvent('popstate');
+        window.dispatchEvent(navaEvent);
     };
 
     return (
